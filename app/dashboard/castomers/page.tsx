45b05957f3e2d798d189987125dfbb485f0446a2
abdulrahman-shawi <aboodi.sy91@gmail.com>
2026-02-01 23:23:50 +0300

    a

---
+++ app/dashboard/castomers/page.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface ICustomrLayoutProps {
+}
+
+const CustomrLayout: React.FunctionComponent<ICustomrLayoutProps> = (props) => {
+  return (
+    <div className="">customer</div>
+  );
+};
+
+export default CustomrLayout;
