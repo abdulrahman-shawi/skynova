69147530f70ffd3e2015cb14544adcff1617e2ea
abdulrahman-shawi <aboodi.sy91@gmail.com>
2026-02-06 01:35:37 +0300

    w

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -523,7 +523,7 @@ const CustomrLayout: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className=" min-h-screen dir-rtl" dir="rtl">
+      <div className="  dir-rtl" dir="rtl">
         <div className=" mx-auto">
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
