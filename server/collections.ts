4091734b24706c6827489c8101c8aecba0dce4a3
abdulrahman-shawi <aboodi.sy91@gmail.com>
2026-09-13 12:55:59 +0300

    بلب

--- server/collections.ts
+++ server/collections.ts
@@ -60,7 +60,7 @@ const getCarrierCollectionBaseAmount = (orderLike: any) => {
 };
 
 const getCarrierCollectionWithShipping = (orderLike: any) => {
-  return getCarrierCollectionBaseAmount(orderLike) + getShippingCharge(orderLike);
+  return Math.max(0, getCarrierCollectionBaseAmount(orderLike) - getShippingCharge(orderLike));
 };
 
 const getCarrierCollectionNetReceived = (orderLike: any) => {
